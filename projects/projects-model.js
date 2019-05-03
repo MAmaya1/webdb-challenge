75ac240e234eff395f0d34d584fc5ea20868f845
Mario Amaya <thefozcat@gmail.com>
2019-05-03 08:51:47 -0700

    added addProject, getProjectById to projects-router

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -1,9 +1,25 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-    getProjects
+    getProjects,
+    getProjectById,
+    addProject
 }
 
 function getProjects() {
     return db('projects')
+}
+
+function getProjectById(id) {
+    return db('projects')
+        .where({ id })
+        .first();
+}
+
+function addProject(project) {
+    return db('projects')
+        .insert(project, 'id')
+        .then(([id]) => {
+            return getProjectById(id)
+        })
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -12,4 +12,32 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    Projects.getProjectById(req.params.id)
+        .then(project => {
+            if (project) {
+                res.status(201).json(project)
+            } else {
+                res.status(404).json({ errorMessage: 'A project with the specified ID does not exist.' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'Could not retrieve project from the database.' })
+        })
+})
+
+router.post('/', (req, res) => {
+    if (!req.body.name || !req.body.description) {
+        res.status(400).json({ errorMessage: 'New projects require both a name and description.' })
+    } else {
+        Projects.addProject(req.body)
+            .then(project => {
+                res.status(201).json(project)
+            })
+            .catch(err => {
+                res.status(500).json({ error: err, message: 'Could not add project to database.' })
+            })
+    }
+})
+
 module.exports = router;
